e46c34bc65ac60bbd1f2c9e388735138a1c667b7
palimi742 <christianpalomo65@gmail.com>
2025-08-14 14:17:37 -0500

    update score header, and addPoints function

--- index.js
+++ index.js
@@ -6,35 +6,20 @@ let homeScoreElement = document.getElementById("home-score");
 console.log(homeScoreElement)
 let guestScoreElement = document.getElementById("guest-score");
 
-function add_one_home(){
-    homeScore += 1;
-    homeScoreElement.textContent = homeScore;
-}
-function add_two_home(){
-    homeScore += 2;
-    homeScoreElement.textContent = homeScore;
-}
-function add_three_home(){
-    homeScore += 3;
-    homeScoreElement.textContent = homeScore;
-}
-
-function add_one_guest(){
-    guestScore += 1;
-    guestScoreElement.textContent = guestScore;
-}
-function add_two_guest(){
-    guestScore += 2;
-    guestScoreElement.textContent = guestScore;
-}
-function add_three_guest(){
-    guestScore += 3;
-    guestScoreElement.textContent = guestScore;
+function addPoints(team, points){
+    if (team == "home"){
+        homeScore += points;
+        homeScoreElement.textContent = homeScore;
+    }
+    else{
+        guestScore += points;
+        guestScoreElement.textContent = guestScore;
+    }
 }
 
 function reset(){
-    guestScore = 0;
     homeScore = 0;
-    guestScoreElement.textContent = guestScore;
+    guestScore = 0;
     homeScoreElement.textContent = homeScore;
+    guestScoreElement.textContent = guestScore;
 }
